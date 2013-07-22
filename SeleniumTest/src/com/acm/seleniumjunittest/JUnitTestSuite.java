9ed8e54457d5364aef8f2109befb13dcae81ea1c
amanampa-test <amanampa.test@gmail.com>
2013-07-22 17:29:18 +0800

    test commit of JUnitTestSuite

---
+++ SeleniumTest/src/com/acm/seleniumjunittest/JUnitTestSuite.java
@@ -0,0 +1,13 @@
+package com.acm.seleniumjunittest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({JUnitTest1.class, JUnitTest2.class, JUnitTest3.class})
+public class JUnitTestSuite {
+}
